861a473521e2beb2f1759ef80b1528f4ad0ca080
a-wtnbe <ayawatanabe714@gmail.com>
2021-05-10 16:53:09 +0900

    5章end

--- jQuery-Lesson/app.js
+++ jQuery-Lesson/app.js
@@ -1,3 +1,20 @@
-$(document).ready(function () {
-  $('body').html('<h1>Hello jQuery!!</h1>');
-});
+// 4章記述
+// $(document).ready(function () {
+//   $('body').html('<h1>Hello jQuery!!</h1>');
+// });
+
+// 青ボックス出現
+$(function(){
+  $('.box1').show();
+  $('.box1').css({'background-color':'#0000FF'});
+  });
+
+// // 上から下に出現
+// $(function(){
+//   $('.box1').slideDown();
+// });
+
+// // 下から上に出現
+// $(function(){
+//   $('.box1').slideUp();
+// });
