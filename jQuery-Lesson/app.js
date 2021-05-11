0a35bea15c8e439ed670db0e2aafa054f2d1b27d
a-wtnbe <ayawatanabe714@gmail.com>
2021-05-12 08:15:27 +0900

    6章end

--- jQuery-Lesson/app.js
+++ jQuery-Lesson/app.js
@@ -3,11 +3,13 @@
 //   $('body').html('<h1>Hello jQuery!!</h1>');
 // });
 
+
+// 5章記述
 // 青ボックス出現
-$(function(){
-  $('.box1').show();
-  $('.box1').css({'background-color':'#0000FF'});
-  });
+// $(function(){
+//   $('.box1').show();
+//   $('.box1').css({'background-color':'#0000FF'});
+//   });
 
 // // 上から下に出現
 // $(function(){
@@ -18,3 +20,33 @@ $(function(){
 // $(function(){
 //   $('.box1').slideUp();
 // });
+
+// マウスカーソルイベントにてCSS変化
+// $(function(){
+//   $('.box1').mouseover(function(){
+//     $('.box1').css({'background-color':'#0000FF'})
+//   });
+//   $('.box1').mouseout(function(){
+//     $('.box1').css({'background-color':'#FF0000'})
+//   });
+// });
+
+// マウスオーバーのクラスを設定しての、適応解除方法
+// $(function(){
+//   $('.box1').mouseover(function(){
+//     $('.box1').addClass('box1-ext');
+//   });
+//   $('.box1').mouseout(function(){
+//     $('.box1').removeClass('box1-ext');
+//   });
+// });
+
+// マウスクリックイベント
+$(function(){
+  $('.box1').on('click',function(){
+    $('.box1').addClass('box1-ext');
+  });
+  $('.box1').mouseout(function(){
+    $('.box1').removeClass('box1-ext');
+  });
+});
